8edff4960894c6cfae5a4da50e2fd8330eaf0bdc
mance <mancelee@126.com>
2024-05-25 10:01:18 +0800

    update

--- src/background/index.js
+++ src/background/index.js
@@ -1,14 +1,14 @@
 import browser from "webextension-polyfill";
 
-console.log("background is running");
 
 const nativeMessageConn = (function () {
     let _nativeMsgPort = null;
     initMsPort();
 
     function initMsPort() {
+        console.log("background is running");
         const nativeHostName = "com.gobot.chrome";
-        if (_nativeMsgPort === undefined) {
+        if (_nativeMsgPort === null) {
             _nativeMsgPort = browser.runtime.connectNative(nativeHostName);
             _nativeMsgPort.onMessage.addListener(onNativeMessage);
             _nativeMsgPort.onDisconnect.addListener(onDisconnected);
@@ -76,3 +76,42 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     nativeMessageConn.sendMsgToHost(request);
     return true;
 });
+/**
+ * Tracks when a service worker was last alive and extends the service worker
+ * lifetime by writing the current time to extension storage every 20 seconds.
+ * You should still prepare for unexpected termination - for example, if the
+ * extension process crashes or your extension is manually stopped at
+ * chrome://serviceworker-internals.
+ */
+let heartbeatInterval;
+
+async function runHeartbeat() {
+    await chrome.storage.local.set({"last-heartbeat": new Date().getTime()});
+}
+
+/**
+ * Starts the heartbeat interval which keeps the service worker alive. Call
+ * this sparingly when you are doing work which requires persistence, and call
+ * stopHeartbeat once that work is complete.
+ */
+async function startHeartbeat() {
+    // Run the heartbeat once at service worker startup.
+    runHeartbeat().then(() => {
+        // Then again every 20 seconds.
+        heartbeatInterval = setInterval(runHeartbeat, 20 * 1000);
+    });
+}
+
+async function stopHeartbeat() {
+    clearInterval(heartbeatInterval);
+}
+
+/**
+ * Returns the last heartbeat stored in extension storage, or undefined if
+ * the heartbeat has never run before.
+ */
+async function getLastHeartbeat() {
+    return (await chrome.storage.local.get("last-heartbeat"))["last-heartbeat"];
+}
+
+startHeartbeat();
